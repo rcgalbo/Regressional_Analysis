86a500d2738f54ce101c5fb17203de858e636c88
unknown <rgalbo@IC300-036.adms.luc.edu>
2015-09-16 11:23:51 -0500

    added anova for lin

--- linear model/St_LinearModel.R
+++ linear model/St_LinearModel.R
@@ -1,8 +1,12 @@
-file <- "P:/STAT308/R_Code/DATA/STREETVN.txt"
+file <- "P:Regressional_Analysis/DATA/STREETVN.txt"
 STREETS <- read.table(file, header=TRUE)
 #scatter plott
 plot(x = STREETS$Hours, y = STREETS$Earnings, xlim = c(5,12), xlab = 'Hours', ylab = 'earnings', main = 'scatter')
 #model regression
 LinMod1 <- lm(STREETS$Hours~STREETS$Earnings)
+
+#returns linear model estimates
 summary(LinMod1)
 
+#generate anova table
+anova(LinMod1)
